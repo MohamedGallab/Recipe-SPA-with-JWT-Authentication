ca94ea04876fd0f3c7d85e10f0cd455a0122551d
Mohamed Gallab <mohamed.w.gallab@gmail.com>
2022-08-26 02:04:46 +0300

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapRazorPages();
+
+app.Run();
